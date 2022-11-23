6e67cbdf9213db751551c7827b2352b023c0708d
tomi2108 <tominegrette@hotmail.com>
2022-11-23 13:52:50 -0300

    refactor(app): refactor services error handling

--- app/src/services/login.ts
+++ app/src/services/login.ts
@@ -1,28 +1,16 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { loginHost } from "./controller";
 
 const url = `${loginHost}/api/login`;
 
 
 export const register = async (email: string, password: string) => {
-  try{
-    const res = await axios.post(`${url}/register`, { email, password });
-    return res.data;
-  }catch(err){
-    if(err instanceof AxiosError){
-      throw new Error(`Request failed: ${err.response?.data.message ?? "No response"}`);
-    } else throw err;
-  }
+  const res = await axios.post(`${url}/register`, { email, password });
+  return res.data;
 };
 
 
 export const loginWithEmailAndPassword = async (email: string, password: string) => {
-  try{
-    const res = await axios.post(url, { email, password });
-    return res.data;
-  }catch(err){
-    if(err instanceof AxiosError){
-      throw new Error(`Request failed: ${err.response?.data.message ?? "No response"}`);
-    } else throw err;
-  }
+  const res = await axios.post(url, { email, password });
+  return res.data;
 };
--- app/src/services/videos.ts
+++ app/src/services/videos.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { VideoVisibility } from "../types";
 import { videoHost } from "./controller.js";
 
@@ -7,15 +7,11 @@ const videoServiceUrl = `${videoHost}/api/videos`;
 
 
 export const uploadVideo = async (file: Blob, visibility: VideoVisibility) => {
-  try{
-    const formdata = new FormData();
-    formdata.append("video", file);
-    formdata.append("visibility", visibility);
-    const res = await axios.post(videoServiceUrl, formdata);
-    return res.data;
-  }catch(err){
-    if(err instanceof AxiosError){
-      throw new Error(err.response?.data.message);
-    } else throw err;
-  }
+
+  const formdata = new FormData();
+  formdata.append("video", file);
+  formdata.append("visibility", visibility);
+
+  const res = await axios.post(videoServiceUrl, formdata );
+  return res.data;
 };
